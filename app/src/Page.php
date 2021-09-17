6dac31007bd28edccb0cf93bf827fb480a803bdb
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-09-17 22:16:27 +1200

    WIP

--- app/src/Page.php
+++ app/src/Page.php
@@ -1,7 +1,7 @@
 <?php
 
 use SilverStripe\CMS\Model\SiteTree;
-use SilverStripe\Forms\TextField;
+
 class Page extends SiteTree
 {
 
--- app/src/PageController.php
+++ app/src/PageController.php
@@ -10,10 +10,16 @@ class PageController extends ContentController
 {
     protected function init()
     {
-        HTTPCacheControlMiddleware::singleton()
-           ->enableCache()
-           ->setMaxAge(0);
+        if(Director::isLive()) {
+            HTTPCacheControlMiddleware::singleton()
+                ->enableCache()
+                ->setMaxAge(0);
 
+        } else {
+            HTTPCacheControlMiddleware::singleton()
+                ->enableCache()
+                ->setMaxAge(60);
+        }
         parent::init();
     }
 }
