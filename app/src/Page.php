fbdfecf4892c6a941c1b499165acd61a175bc0be
nicolaas@ninasimone <nt@sunnysideup.co.nz>
2021-10-01 13:59:54 +1300

    WIP

--- app/src/Page.php
+++ app/src/Page.php
@@ -1,8 +1,20 @@
 <?php
 
 use SilverStripe\CMS\Model\SiteTree;
+use Sunnysideup\CleanerTinyMCEConfig\Forms\HTMLEditorFieldShort;
 
 class Page extends SiteTree
 {
 
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->addFieldsToTab(
+            'Root.Main',
+            [
+                HTMLEditorFieldShort::create('Content')
+            ]
+        );
+        return $fields;
+    }
 }
