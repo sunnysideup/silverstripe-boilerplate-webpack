b653e40e43bd85d4912b0a8ee74c932067176b53
Nicolaas <github@sunnysideup.co.nz>
2022-06-10 20:50:55 +1200

    WIP

--- bin/npm-commit.sh
+++ bin/npm-commit.sh
@@ -1,3 +1,2 @@
-cd ..
 cd themes/sswebpack_engine_only/ && npm run build --theme_dir=themes/base && cd - && git add . -A && git commit . -m "WIP" && git push
 cd themes/base &&  git add . -A && git commit . -m "WIP" && git push && cd -
